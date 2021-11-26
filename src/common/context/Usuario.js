6e4389bd2c5114358d1bf680aa8db29df42fe6d0
Alexandra <bguevaraale@gmail.com>
2021-11-25 22:22:30 -0300

    Projeto SPA com Context do login

---
+++ src/common/context/Usuario.js
@@ -0,0 +1,15 @@
+import React, { createContext, useState } from "react";
+
+export const UsuarioContext = createContext();
+
+
+export const UsuarioProvider = ({ children }) => { //props.children = é a props default react dos componentes que passar pro provider
+  const [nome, setNome] = useState("");
+  const [saldo, setSaldo] = useState(0);
+  return (
+    <UsuarioContext.Provider value={{ nome, setNome, saldo, setSaldo }}>
+      {children} 
+    </UsuarioContext.Provider>
+  );
+};
+ 
---
+++ src/routes.js
@@ -0,0 +1,27 @@
+import Login from "./pages/Login";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Feira from "pages/Feira";
+import Carrinho from "pages/Carrinho";
+import { UsuarioProvider } from "./common/context/Usuario";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <UsuarioProvider>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/feira">
+            <Feira />
+          </Route>
+        </UsuarioProvider>
+        <Route path="/carrinho">
+          <Carrinho />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
